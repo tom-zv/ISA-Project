807aa7507b3476e6266de6f21f90416c5407a9e8
Tom Zayev <Tom.zayev@gmail.com>
2021-12-27 16:25:18 +0200

    27.12 - instruction 6 -15 implemented

--- Simulator/main.c
+++ Simulator/main.c
@@ -94,28 +94,28 @@ int hex_to_dec(char* hex_data, int hex_size, int* hex_index) {
 	return decimal;
 }
 
-// converts dec to binary, storing the resultant binary string in indicated array.
-void dec_to_bin(int decimal, char* binary) {
+// converts dec to binary, storing the resultant signed binary string in indicated array.
+void dec_to_bin(int decimal, char* binary, int len) {
 
 	int i, j;
 
-	for (i = WORD - 1; i >= 0; i--) {
+	for (i = len - 1; i >= 0; i--) {
 
 		j = decimal >> i;
 
 		if (j & 1) {
-			binary[WORD - 1 - i] = '1';
+			binary[len - 1 - i] = '1';
 		}
 		else
-			binary[WORD - 1 - i] = '0';
+			binary[len - 1 - i] = '0';
 	}
 
-	//binary[WORD] = '\0';   // for debug prints
+	//binary[len] = '\0';   // for debug prints
 
 	return;
 }
 
-//implementation of strtol for signed binary
+// implementation of strtol for signed binary
 int signed_binary_strtol(char *str) {
 	
 	if (str[0] == '1') {   // adjusted conversion for negative numbers.
@@ -134,15 +134,60 @@ int signed_binary_strtol(char *str) {
 
 		}
 
-		int num = - strtol(flip_str, NULL, 2) - 1 ;   // strtol of flipped bits, minus one, gives us the correct conversion for signed negative numbers.
+		int num = -strtol(flip_str, NULL, 2) - 1 ;   // strtol of flipped bits, minus one, gives us the correct conversion for signed negative numbers.
 		return num;
-			
 	}
 
 
 	int num = strtol(str, NULL, 2);
 	return num;
 
+}
+
+// manual srl 
+int right_logical_shift(char* binary, int shamt) {
+
+	if (shamt == 0) {
+		return signed_binary_strtol(binary);
+	}
+
+	int i,j;
+	char shifted_binary[WORD + 1];
+
+	for (i = 0; i < shamt; i++) {
+
+		for (j = WORD - 1; j > 0; j--) {
+
+			shifted_binary[j] = binary[j - 1];
+
+		}
+
+		shifted_binary[0] = '0';
+	}
+
+	return signed_binary_strtol(shifted_binary);
+}
+
+// increment PC by one.
+void increment_PC(char *PC) {
+
+	int PC_int = strtol(PC, NULL, 2);
+
+	PC_int += 1;
+	dec_to_bin(PC_int, PC, 12);
+
+
+}
+
+// set PC to indicated binary string, taking the lower 12 bits.
+void set_PC(char* PC, char* binary) {
+
+	int i;
+	
+	for (i = 0; i < 12; i++) {
+
+		PC[i] = binary[WORD - 12 + i] ;
+	}
 }
 
 // decodes the given instruction from hex to dec, storing the results in the given array of instruction fields.
@@ -163,8 +208,11 @@ void decode_instruction(char* hex_data, int* field_array) {
 	//printf(" %d | %d | %d | %d | %d | %d | %d |\n", opcode, rd, rs, rt, rm, imm1, imm2);
 }
 
+
+
+
 // executes an instruction.
-void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char R[NUM_OF_REGISTERS][WORD + 1], int PC) {
+void execute_instruction(int *instruction_fields_array, char R[NUM_OF_REGISTERS][WORD + 1], char *PC) {
 
 	int opcode = instruction_fields_array[0];
 	int rd = instruction_fields_array[1];
@@ -174,8 +222,8 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 	int imm1 = instruction_fields_array[5];
 	int imm2 = instruction_fields_array[6];
 
-	dec_to_bin(imm1, R[1]);		//  store imm1 and imm2 in their respective registers.
-	dec_to_bin(imm2, R[2]);    //
+	dec_to_bin(imm1, R[1], WORD);		//  store imm1 and imm2 in their respective registers.
+	dec_to_bin(imm2, R[2], WORD);    //
 
 	int result;
 
@@ -183,12 +231,12 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 
 	switch (opcode) {
 
-	case 0:  //add
+	/*case 0:  //add
 
 		printf("opcode %d, instruction: add\n", opcode);
 
 		result = signed_binary_strtol(R[rs]) + signed_binary_strtol(R[rt]) + signed_binary_strtol(R[rm]);
-		dec_to_bin(result, R[rd]);
+		dec_to_bin(result, R[rd], WORD);
 
 		//R[rd][WORD] = '\0';
 		//printf("result %s\n", R[rd]);
@@ -201,7 +249,7 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 		printf("opcode %d, instruction: sub\n", opcode);
 
 		result = signed_binary_strtol(R[rs]) - signed_binary_strtol(R[rt]) - signed_binary_strtol(R[rm]);
-		dec_to_bin(result, R[rd]);
+		dec_to_bin(result, R[rd], WORD);
 
 		//R[rd][WORD] = '\0';
 		//printf("result %s\n", R[rd]);
@@ -212,7 +260,7 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 		printf("opcode %d, instruction: mac\n", opcode);
 
 		result = signed_binary_strtol(R[rs]) * signed_binary_strtol(R[rt]) - signed_binary_strtol(R[rm]);
-		dec_to_bin(result, R[rd]);
+		dec_to_bin(result, R[rd], WORD);
 
 		//R[rd][WORD] = '\0';
 		//printf("result %s\n", R[rd]);
@@ -223,7 +271,7 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 		printf("opcode %d, instruction: and\n", opcode);
 		
 		result = signed_binary_strtol(R[rs]) & signed_binary_strtol(R[rt]) & signed_binary_strtol(R[rm]);
-		dec_to_bin(result, R[rd]);
+		dec_to_bin(result, R[rd], WORD);
 
 		//R[rd][WORD] = '\0';
 		//printf("result %s\n", R[rd]);
@@ -234,7 +282,7 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 		printf("opcode %d, instruction: or\n", opcode);
 		
 		result = signed_binary_strtol(R[rs]) | signed_binary_strtol(R[rt]) | signed_binary_strtol(R[rm]);
-		dec_to_bin(result, R[rd]);
+		dec_to_bin(result, R[rd], WORD);
 
 		//R[rd][WORD] = '\0';
 		//printf("result %s\n", R[rd]);
@@ -245,7 +293,7 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 		printf("opcode %d, instruction: xor\n", opcode);
 
 		result = signed_binary_strtol(R[rs]) ^ signed_binary_strtol(R[rt]) ^ signed_binary_strtol(R[rm]);
-		dec_to_bin(result, R[rd]);
+		dec_to_bin(result, R[rd], WORD);
 
 		//R[rd][WORD] = '\0';
 		//printf("result %s\n", R[rd]);
@@ -255,8 +303,8 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 
 		printf("opcode %d, instruction: sll\n", opcode);
 		
-		result = signed_binary_strtol(R[rs]) << signed_binary_strtol(R[rt]);
-		dec_to_bin(result, R[rd]);
+		result = signed_binary_strtol(R[rs]) << signed_binary_strtol(R[rt]);                                    // sll - shifting is cyclical, 32 is a zero shift.
+		dec_to_bin(result, R[rd], WORD);
 
 		//R[rd][WORD] = '\0';
 		//printf("result %s\n", R[rd]);
@@ -267,49 +315,96 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 
 		printf("opcode %d, instruction: sra\n", opcode);
 
-		result = strtol(R[rs], NULL, 2) << strtol(R[rt], NULL, 2);
-		dec_to_bin(result, R[rd]);
-
+		result = signed_binary_strtol(R[rs]) >> signed_binary_strtol(R[rt]);                       // sra - for positive nums, reaches 0 (000...) and stays there for increased shift amounts.
+		dec_to_bin(result, R[rd], WORD);																			   //       for negative nums, reaches -1 (111...) and stays there,
 		break;
 
 	case 8: //srl
 
 		printf("opcode %d, instruction: srl\n", opcode); 
-		break;
+
+		result = right_logical_shift(R[rs], signed_binary_strtol(R[rt]));
+		dec_to_bin(result, R[rd], WORD);
+
+		break; */
+		
+		
 
 	case 9: //beq
 
 		printf("opcode %d, instruction: beq\n", opcode);
+
+		if (signed_binary_strtol(R[rs]) == signed_binary_strtol(R[rt])) {
+			
+			set_PC(PC, R[rm]);
+		}
+
 		break;
 
 	case 10: //bne
 
 		printf("opcode %d, instruction: bne\n", opcode);
+
+		if (signed_binary_strtol(R[rs]) != signed_binary_strtol(R[rt])) {
+
+			set_PC(PC, R[rm]);
+		}
+
 		break;
 
 	case 11: //blt
 
 		printf("opcode %d, instruction: blt\n", opcode);
+
+		if (signed_binary_strtol(R[rs]) < signed_binary_strtol(R[rt])) {
+
+			set_PC(PC, R[rm]);
+		}
+
 		break;
 
 	case 12: //bgt
 
 		printf("opcode %d, instruction: bgt\n", opcode);
+
+		if (signed_binary_strtol(R[rs]) > signed_binary_strtol(R[rt])) {
+
+			set_PC(PC, R[rm]);
+		}
+
 		break;
 
 	case 13: //ble
 
 		printf("opcode %d, instruction: ble\n", opcode);
+
+		if (signed_binary_strtol(R[rs]) <= signed_binary_strtol(R[rt])) {
+
+			set_PC(PC, R[rm]);
+		}
+
 		break;
 
 	case 14: //bge
 
 		printf("opcode %d, instruction: bge\n", opcode);
+
+		if (signed_binary_strtol(R[rs]) >= signed_binary_strtol(R[rt])) {
+
+			set_PC(PC, R[rm]);
+		}
+
 		break;
 
 	case 15: //jal
 
 		printf("opcode %d, instruction: jal\n", opcode);
+
+		result = strtol(PC, NULL, 2) + 1;
+		dec_to_bin(result, R[rd], WORD);
+		
+		set_PC(PC, R[rm]);
+
 		break;
 
 	case 16: //lw
@@ -349,51 +444,69 @@ void execute_instruction(int *instruction_fields_array[NUM_OF_INST_FIELDS], char
 
 
 
-
 void main(int argc, char* argv[]) {
 
-	char* imemin, * dmemin;
+	char *imemin, *dmemin;
 
 	imemin = readfile(argv[1], IMEM_LINE_SIZE);  // contains data in sequence, imemin[ i * data_size] for the i+1 line start address.
 	dmemin = readfile(argv[2], DMEM_LINE_SIZE);  
 
-	int PC = 0; // change to 12 bit binary register?
+	char PC[12 + 1 ];
+	PC[12] = '\0'; 
+
+	dec_to_bin(0, PC, 12);
+
 	char registers[NUM_OF_REGISTERS][WORD+1]; // +1 for null character used in debugging prints. TODO
 	int inst_field_array[NUM_OF_INST_FIELDS];
 
 	//
 
-	int dec = 127;
-	dec_to_bin(dec, registers[7]);   // manual registry assignments for testing. 
+	int dec = 2;
+	dec_to_bin(dec, registers[7], WORD);   // manual registry assignments for testing. 
 
-	dec = 32;
-	dec_to_bin(dec, registers[8]);
+	//registers[7][WORD] = '\0';
+	//printf("work reg = %s\n", registers[7]);
 
-	
-    
+	dec = 2;
+	dec_to_bin(dec, registers[8], WORD);
+
+	dec = 8192;
+	dec_to_bin(dec, registers[9], WORD);
 	
 	//
 
-	
-	int tmp_cond = 1; // while loop exit condition, temporary for controlling how many instructions are ran.
 
+	
+	//-----------------------------------------------------------------------------------------------------------------\\
+	//-----------------------------------------------------------------------------------------------------------------\\	
+
+	int tmp_cond = 1; // while loop exit condition, temporary for controlling how many instructions are ran.
 	while (tmp_cond != 2) {
 
 
-		decode_instruction(imemin + PC * IMEM_LINE_SIZE, inst_field_array);
+		decode_instruction(imemin + strtol(PC, NULL, 2) * IMEM_LINE_SIZE, inst_field_array);
 
-	/*	int i;
+	/*	int i;																												
 		for (i = 0; i < NUM_OF_INST_FIELDS; i++) {
-			printf(" %d |", inst_field_array[i]);
+			printf(" %d |", inst_field_array[i]);                                                                            Processor running loop
 		}
 		printf("\n");*/
 
-		execute_instruction(&inst_field_array, &registers, PC);
+		execute_instruction(&inst_field_array, &registers, &PC);
 
-		PC++;  //  TODO add logic to increment PC here OR from the instruction.
+		printf("PC = %s\n", PC);
+		increment_PC(PC);  //  TODO add logic to increment PC here OR from the instruction.
 		tmp_cond++;
 	}
+	
+	
+	
+
+	//-----------------------------------------------------------------------------------------------------------------\\
+	//-----------------------------------------------------------------------------------------------------------------\\
+
 
 	registers[3][WORD] = '\0';              // print result in v0, for debugging.
-	printf("result %s\n", registers[3]);
+	//printf("\nresult\nsigned binary: %s  |  dec: %d\n", registers[3], signed_binary_strtol(registers[3]));
+
 }
